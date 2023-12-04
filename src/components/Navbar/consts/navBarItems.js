8f1b0037c2d236aa970cc609c4bdc4182ebde731
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-04 14:44:48 +0100

    init from main repo

---
+++ src/components/Navbar/consts/navBarItems.js
@@ -0,0 +1,28 @@
+export const mainNavBarItems = [
+    {
+        id: 0,
+        label: 'What is HiveBuddy?',
+        route: 'what-is-hiveBuddy',
+    },
+    {
+        id: 1,
+        label: 'Our team',
+        route: 'our-team',
+    },
+    {
+        id: 2,
+        label: 'Our Strengths',
+        route: 'strengths',
+    },
+    {
+        id: 3,
+        label: 'How is it done?',
+        route: 'how-is-it-done',
+    },
+    // {
+    //     id: 4,
+    //     icon: '',
+    //     label: 'smth',
+    //     route: 'route',
+    // }
+]

58119f73ecf653fd5fe01c97d984afb0680e7bc0
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-05 16:50:12 +0100

    realtime data via SockJs in Overview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import HiveBuddy from "./pages/HiveBuddy/HiveBuddy";
+import Overview from './pages/Overview';
 import Process from "./pages/Process/Process";
 import Strengths from "./pages/Strengths/Strengths";
 import Team from "./pages/Team/Team";
@@ -16,7 +17,8 @@ function App() {
             {/*<Team/>*/}
             {/*<Strengths/>*/}
             {/*}*/}
-            <Process/>
+            {/* <Process/> */}
+            <Overview/>
         </div>
     );
 }
---
+++ src/pages/Overview/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Client } from '@stomp/stompjs';
+
+const SOCKET_URL = 'ws://localhost:8080/ws-message';
+
+class Overview extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        messages: 'You server message here.',
+      };
+    };
+  
+    componentDidMount() {
+      let currentComponent = this;
+      let onConnected = () => {
+        console.log("Connected!!")
+        client.subscribe('/overview/1', function (msg) {
+          if (msg.body) {
+            var jsonBody = JSON.parse(msg.body);
+            if (jsonBody.value) {
+              currentComponent.setState({ messages: jsonBody.value })
+            }
+          }
+        });
+      }
+  
+      let onDisconnected = () => {
+        console.log("Disconnected!!")
+      }
+  
+      const client = new Client({
+        brokerURL: SOCKET_URL,
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+        onConnect: onConnected,
+        onDisconnect: onDisconnected
+      });
+  
+      client.activate();
+    };
+  
+    render() {
+      return (
+        <div>
+          <div>{this.state.messages}</div>
+        </div>
+      );
+    }
+  
+  }
+  
+  export default Overview;

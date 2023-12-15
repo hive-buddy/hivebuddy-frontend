f6108cf5ab230303f5f96fa5f790c3d588293854
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-15 07:45:30 +0100

    realtime data working in Overview.js

--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -1,52 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import SockJsClient from 'react-stomp';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import SensorButton from '../../components/SensorButton/SensorButton';
 
 const SOCKET_URL = 'http://localhost:8080/ws-message';
 
 const Overview = () => {
 
-    const sensorTypeIds = [1, 2, 3, 4, 5];
-    const valuesArray = [];
-    sensorTypeIds.forEach((sensorId) => {
-        valuesArray.push([sensorId, 0])
-    });
 
-    const [values, setValues] = useState([0]);
+    // const sensorTypeIds = [1, 2, 3, 4, 5];
+    // const valuesArray = [];
+    // sensorTypeIds.forEach((sensorId) => {
+    //     valuesArray.push([sensorId, 0]);
+    // });
+
+    // const [values, setValues]= useState(new Map());
+    const [sensorOne, setSensorOne] = useState({});
+    const [sensorTwo, setSensorTwo] = useState({});
+    const [sensorThree, setSensorThree] = useState({});
+    const [sensorFour, setSensorFour] = useState({});
+    const [sensorFive, setSensorFive] = useState({});
 
     let onConnected = () => {
-        console.log("Connected!!")
+        console.log("Connected!!");
     }
 
+    // let onDisconnected = () => {
+    //     console.log("Disconnected");
+    // }
+
     let onValuesReceived = (data) => {
-        const receivedData = data.message;
-        setValues(data.message)
-        setValues([...values, receivedData]);
-        // setValues((prevSensorData) => ({
-        //     ...prevSensorData,
-        //     [receivedData.sensorId]: {
-        //         sensorId: receivedData.sensorId,
-        //         value: receivedData.value,
-        //     },
-        // }));
+        // console.log("Received data:");
+        console.log(data);
+        // console.log(data.message);
+        // console.log(data.message.map((obj) => {obj.value}));
+        // console.log(Object.keys(data));
+        // const receivedData = data.message;
+        // setValues(Object.entries(data));
+        // console.log(values);
+        switch (data.sensorTypeId) {
+            case 1:
+                setSensorOne(data);
+                break;
+            case 2:
+                setSensorTwo(data);
+                break;
+            case 3:
+                setSensorThree(data);
+                break;
+            case 4:
+                setSensorFour(data);
+                break;
+            case 5:
+                setSensorFive(data);
+                break;
+            default:
+                return;
+        }
     }
+
+    
 
     return (
         <div>
             <SockJsClient
                 url={SOCKET_URL}
-                topics={['/topic/message']}
+                topics={['/topic/overview/1/1']}
                 onConnect={onConnected}
                 onDisconnect={console.log("Disconnected!")}
                 onMessage={data => onValuesReceived(data)}
                 debug={false}
             />
-            <div>
-                {values}
-                {/*))} {Array.isArray(values) && values.map((value, index) => (*/}
-                {/*<li key={index}>*/}
-                {/*    {value}*/}
-                {/*    <*/}
-            </div>
+            <ButtonGroup fullWidth="true" size="large" variant="text" aria-label="text button group">
+                <SensorButton sensor={sensorOne} />
+                <SensorButton sensor={sensorTwo} />
+                <SensorButton sensor={sensorThree} />
+                <SensorButton sensor={sensorFour} />
+                <SensorButton sensor={sensorFive} />
+            </ButtonGroup>
         </div>
     );
 }
--- src/pages/Process/Process.js
+++ src/pages/Process/Process.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import ScrollAnimation from 'react-animate-on-scroll'
-const SOCKET_URL = 'http://localhost:8080/ws-message';
+// import React from 'react'
+// // import ScrollAnimation from 'react-animate-on-scroll'
+// const SOCKET_URL = 'http://localhost:8080/ws-message';
 
 
-const Animation = () => (
-    <ScrollAnimation animateIn="fadeIn">Some Text</ScrollAnimation>
-)
+// const Animation = () => (
+//     // <ScrollAnimation animateIn="fadeIn">Some Text</ScrollAnimation>
+// )
 
-const Process = () => {
-    return (
-        <div>Process</div>
+// const Process = () => {
+//     return (
+//         <div>Process</div>
 
-    )
-}
-export default Process
+//     )
+// }
+// export default Process
